339a8fa176d408ca9d633acc6296aaf53d45b93c
NazmeenBegum <shaiknazmeen1991@gmail.com>
2024-09-18 10:51:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,243 @@
+let btn = document.getElementById('download')
+
+btn.addEventListener('click',myfun)
+
+function mydown(){
+    html2canvas(document.getElementById('naaz')).then(canvas => {
+        const link=document.createElement('a')
+        link.href=canvas.toDataURL('image/png')
+        link.download='image.png'
+        link.click();  
+    });
+}
+
+
+
+
+function mya() {
+ document.getElementById("naaz").style.backgroundColor='aqua'
+    
+}
+function myb() {
+    
+    document.getElementById('naaz').style.backgroundColor='blue'
+    
+}
+function myc() {
+    
+    document.getElementById('naaz').style.backgroundColor='pink'
+    
+}
+function myd() {
+    
+    document.getElementById('naaz').style.backgroundColor='green'
+    
+}
+function mye() {
+    
+    document.getElementById('naaz').style.backgroundColor='red'
+    
+}
+function myf() {
+    
+    document.getElementById('naaz').style.backgroundColor='yellow'
+    
+}
+function myg() {
+    
+    document.getElementById('naaz').style.backgroundColor='gray'
+    
+}
+function myh() {
+    
+    document.getElementById('naaz').style.backgroundColor='orange'
+    
+}
+function myi() {
+    
+    document.getElementById('naaz').style.backgroundColor='purple'
+    
+}
+function myj() {
+    
+    document.getElementById('naaz').style.backgroundColor='lime'
+    
+}
+function myk() {
+    
+    document.getElementById('naaz').style.backgroundColor='coral'
+    
+}
+function myl() {
+    
+    document.getElementById('naaz').style.backgroundColor='navy'
+    
+}
+function mym() {
+    
+    document.getElementById('naaz').style.backgroundColor='blueviolet'
+    
+}
+function myn() {
+    
+    document.getElementById('naaz').style.backgroundColor='teal'
+    
+}
+function myo() {
+    
+    document.getElementById('naaz').style.backgroundColor='black'
+    
+}
+function myp() {
+    
+    document.getElementById('naaz').style.backgroundColor='goldenrod'
+    
+}
+function myq() {
+    
+    document.getElementById('naaz').style.backgroundColor='brown'
+    
+}
+function myr() {
+    
+    document.getElementById('naaz').style.backgroundColor='wheat'
+    
+}
+
+
+
+function myaa(){
+    document.getElementById('naaz').style.color='sienna'
+    
+}
+function mybb() {
+    
+    document.getElementById('naaz').style.color='blue'
+    
+}
+function myoli(){
+    
+    document.getElementById('naaz').style.color='olive'
+    
+    
+}
+function mydd() {
+    
+    document.getElementById('naaz').style.color='green'
+    
+}
+function myee() {
+    
+    document.getElementById('naaz').style.color='red'
+    
+}
+function myff() {
+    
+    document.getElementById('naaz').style.color='yellow'
+    
+}
+function mygg() {
+    
+    document.getElementById('naaz').style.color='gray'
+    
+}
+function myhh() {
+    
+    document.getElementById('naaz').style.color='orange'
+    
+}
+function myii() {
+    
+    document.getElementById('naaz').style.color='purple'
+    
+}
+function myjj() {
+    
+    document.getElementById('naaz').style.color='lime'
+    
+}
+function mykk() {
+    
+    document.getElementById('naaz').style.color='coral'
+    
+}
+function myll() {
+    
+    document.getElementById('naaz').style.color='navy'
+    
+}
+function mymm() {
+    
+    document.getElementById('naaz').style.color='blueviolet'
+    
+}
+function mynn() {
+    
+    document.getElementById('naaz').style.color='teal'
+    
+}
+function myoo() {
+    
+    document.getElementById('naaz').style.color='black'
+    
+}
+function mypp() {
+    
+    document.getElementById('naaz').style.color='goldenrod'
+    
+
+}
+function myqq() {
+    
+    document.getElementById('naaz').style.color='brown'
+    
+}
+function myrr() {
+    
+    document.getElementById('naaz').style.color='wheat'
+    
+}
+function mybold(){
+    document.getElementById("naaz").style.fontFamily='cursive'
+}
+function myfun2(){
+    document.getElementById("naaz").style.fontFamily='arial'
+}
+function myfun3(){
+    document.getElementById('naaz').style.fontFamily='italic'
+}
+function myfun4(){
+    document.getElementById('naaz').style.fontFamily='Verdana, Geneva, Tahoma, sans-serif'
+}
+function myfun5(){
+    document.getElementById('naaz').style.fontFamily='Courier New, Courier, monospace'
+}
+function myfun6(){
+    document.getElementById('naaz').style.fontFamily='bodoni'
+}
+function myjan(){
+    document.getElementById('naaz').style.fontFamily='Impact, sans-serif'
+}
+function myfun8(){
+    document.getElementById('naaz').style.fontFamily='belwe'
+}
+function myfun9(){
+    document.getElementById('naaz').style.fontFamily='Segoe UI, Tahoma, Geneva, Verdana, sans-serif'
+}
+function myfun10(){
+    document.getElementById('naaz').style.fontFamily='Trebuchet MS, Lucida Sans Unicode, Lucida Grande, Lucida Sans, Arial, sans-serif'
+}
+function myfun11(){
+    document.getElementById('naaz').style.fontFamily='Cambria, Cochin, Georgia, Times, "Times New Roman", serif'
+}
+function myfun12(){
+    document.getElementById('naaz').style.fontFamily=' Impact, sans-serif'
+}
+function myreset(){
+    document.getElementById('naaz').style.backgroundColor='white'
+}
+
+function myclear(){
+    document.getElementById('naaz').style.color='white'
+}
+
